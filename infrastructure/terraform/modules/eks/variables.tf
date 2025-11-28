59bb03caf136d80716d26b13c4b0d3d0df534157
nikhilmathur1957 <nikhilmathur1957@gmail.com>
2025-11-28 16:49:52 +0530

    Kubernetes cluster version updaate

--- infrastructure/terraform/modules/eks/variables.tf
+++ infrastructure/terraform/modules/eks/variables.tf
@@ -6,7 +6,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version for EKS cluster"
   type        = string
-  default     = "1.28"
+  default     = "1.29"
 }
 
 variable "subnet_ids" {
